b863403636217a7208da0b1626dfbe29662d3f31
Christen Weston <paleeserecycle@gmail.com>
2021-10-31 15:57:13 -0700

    Added in comment sfor the scripts.js and also a third field for who the sent the letter

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
-  //Name is the name of the form in this instance
+  //Name is the name of the form in this instance. The submit button in this form adds an event
+  //listener
   $("#name").submit(function(event) {
     //prevent default ensures that the results of the letter show up once submit is entered
+    //Here's all the things that happen once the submit button is pressed:
     event.preventDefault();
+
+    //Create variables for all of the inputs. Set them equal to the input#id.val()
     const name1Input = $("input#name1").val();
     const greeting =$("input#greeting").val();
+    const fromWho =$("input#fromWho").val();
 
+    //If the form is re-submitted w/ new values in the form this will ensure that
+    //only the new values are used (by using text instead of append)
     $(".name1").text(name1Input);
     $(".greeting").text(greeting);
+    $(".fromWho").text(fromWho);
+
+    //Finally, show that letter w/ the inputs used!
     $("#letter").show();
   });
 });
