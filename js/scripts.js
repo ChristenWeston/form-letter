d9ac88d56347ab1cd68137b2137de232354d72b9
Christen Weston <paleeserecycle@gmail.com>
2021-10-31 11:57:38 -0700

    updated styles and scripts.js to pertain to the letter

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#name").submit(function(event) {
+    event.preventDefault();
+    const name1Input = $("input#name1").val();
+
+    $(".name1").text(name1Input);
+
+    $("#letter").show();
+  });
+});
