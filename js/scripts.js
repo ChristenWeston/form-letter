e0d670cfbfa17ffaa5b1e03f68674d0bd99e870a
Christen Weston <paleeserecycle@gmail.com>
2021-10-31 15:00:49 -0700

    Added in additional field for the post card greeting

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
+  //Name is the name of the form in this instance
   $("#name").submit(function(event) {
+    //prevent default ensures that the results of the letter show up once submit is entered
     event.preventDefault();
     const name1Input = $("input#name1").val();
+    const greeting =$("input#greeting").val();
 
     $(".name1").text(name1Input);
+    $(".greeting").text(greeting);
     $("#letter").show();
   });
 });
